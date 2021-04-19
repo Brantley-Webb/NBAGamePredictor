e07d7b81fd3ba33edf06f041c207ed3680aed053
Brantley Webb <bman4tw@hotmail.com>
2021-04-19 01:58:09 -0500

    Results from SVM Data Analysis

--- SupportVectorMachine.py
+++ SupportVectorMachine.py
@@ -5,15 +5,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+import scikitplot as skplt
 
 
 class SupportVectorMachine:
 
-    def test_train_dataframe_split(self, file_name, subsample_size):
+    def test_train_dataframe_split(self, file_name, subsample_size, train_test_percentage):
         aggregated_game_stats_for_season = pd.read_csv(str(file_name))
         aggregated_game_stats_for_season.sample(frac=subsample_size)
-        train_dataframe, test_dataframe = train_test_split(aggregated_game_stats_for_season, test_size=0.2,
+        train_dataframe, test_dataframe = train_test_split(aggregated_game_stats_for_season, test_size=train_test_percentage,
                                                            shuffle=True)
         return train_dataframe, test_dataframe
 
@@ -25,10 +28,10 @@ class SupportVectorMachine:
             print(
                 f'The away team: {away_team} have a probability of {win_probability} of beating the home team: {home_team}.')
 
-    def display_accuracy(self, y_test, y_prediction, max_iterations, file):
+    def display_accuracy(self, y_test, y_prediction, c_value, file):
         accuracy = accuracy_score(y_test, np.round(y_prediction))
-        print("The accuracy of the model was: " + str(accuracy) + " with max iterations of " + str(max_iterations))
-        file.write(str(accuracy) + ", " + str(max_iterations) + "\n")
+        print("The accuracy of the model was: " + str(accuracy) + " with c value of " + str(c_value))
+        file.write(str(accuracy) + "\n")
         return accuracy
 
 
@@ -38,11 +41,12 @@ if __name__ == '__main__':
     SVM = SupportVectorMachine()
     our_subsample_size = 1.0
     our_max_iterations = 1000
-    our_increasing_factor = 1000
+    our_test_split = 0.20
+    our_c = 1.0
     file = open("accuracy_results.txt", "a+")
     file.truncate(0)
-    while our_max_iterations <= 1000:
-        our_train_dataframe, our_test_dataframe = SVM.test_train_dataframe_split(our_file_name, our_subsample_size)
+    for i in range(50):
+        our_train_dataframe, our_test_dataframe = SVM.test_train_dataframe_split(our_file_name, our_subsample_size, our_test_split)
 
         X_train = our_train_dataframe.drop(
             columns=['away_name', 'away_abbr', 'home_name', 'home_abbr', 'week', 'result'])
@@ -50,19 +54,13 @@ if __name__ == '__main__':
         X_test = our_test_dataframe.drop(
             columns=['away_name', 'away_abbr', 'home_name', 'home_abbr', 'week', 'result'])
         y_test = our_test_dataframe[['result']]
-        # scaler = StandardScaler()
-        # scaler.fit(X_train)
-        # scaler.transform(X_train)
-        # scaler.fit(X_test)
-        # scaler.transform(X_test)
 
-        result = LinearSVC(max_iter=our_max_iterations)
+        scaler_and_svm = make_pipeline(LinearSVC(max_iter=our_max_iterations, C=our_c))
 
-        result.fit(X_train, np.ravel(y_train.values))
-        y_pred = result.predict(X_test)
+        scaler_and_svm.fit(X_train, np.ravel(y_train.values))
+        y_pred = scaler_and_svm.predict(X_test)
 
-        accuracy = SVM.display_accuracy(y_test, y_pred, our_max_iterations, file)
+        accuracy = SVM.display_accuracy(y_test, y_pred, our_c, file)
 
-        our_max_iterations = our_max_iterations + our_increasing_factor
 
     file.close()
